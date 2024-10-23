186c0e84e5e54503828d09fb2c9d4500c46650a0
Shubh4151 <agaleshubhangi20@gmail.com>
2024-10-23 17:51:24 +0530

    Update objects.js

--- objects.js
+++ objects.js
@@ -70,12 +70,13 @@
 // }
 // console.log(obj)
 
-
+////////// Array of Objects ///////////
 // let a=[{"pid":101,"name":"com","price":98000},
 //     {"pid":102,"name":"mobile","price":38000},
-//     {"pid":103,"name":"mouse","price":98000},
-//     {"pid":104,"name":"tab","price":98000}]
+//     {"pid":103,"name":"mouse","price":980},
+//     {"pid":104,"name":"tab","price":20000}]
 
+    //show object elements
 // for(let i in a)
 // {
 //     console.log(i,a[i])
@@ -88,14 +89,61 @@
 //     console.log("-------")
 // }
 
-//Display product details whose price >25000
-for(let i in a)
-{
-    if(a[i].price>25000)
+    //Display product details whose price >25000
+// for(let i in a)
+// {
+//     if(a[i].price>25000)
+//     {
+//         console.log(a[i].pid)
+//         console.log(a[i].name)
+//         console.log(a[i].price)
+//         console.log("---")
+//     }
+// }
+
+    //Delete product with name tab
+// for(let i in a)
+// {
+//     if(a[i].name=='tab')
+//     {
+//         a.splice(i,1)
+//         break
+//     }
+// }
+// console.log(a)
+
+
+
+
+//Q..  Create object of student object
+
+let a=[{"sid":101,"name":"Anu","dept":"etc","marks":93},
+    {"sid":102,"name":"shubh","dept":"cse","marks":80},
+    {"sid":103,"name":"Anushu","dept":"mech","marks":38},
+    {"sid":104,"name":"shAnu","dept":"civil","marks":73},
+    {"sid":105,"name":"Ansh","dept":"ece","marks":53}]
+    
+    for(let i in a)
     {
-        console.log(a[i].pid)
-        console.log(a[i].name)
-        console.log(a[i].price)
-        console.log("---")
+        if(a[i].marks>=70&&a[i].marks<=80)
+        {
+            console.log(1,a[i])
+            console.log("...........")
+        }
+    }      
+    for(let i in a)
+    {
+        if(a[i].dept=="cse" || a[i].dept=="ece")
+            {
+                console.log(2,a[i])
+                console.log("...........")
+            }
+    } 
+    for(let i in a)
+    { 
+        if(a[i].dept=="mech" || a[i].dept=="civil")
+        {
+            a.splice(i,2)
+        }
     }
-}
+    console.log(a)
